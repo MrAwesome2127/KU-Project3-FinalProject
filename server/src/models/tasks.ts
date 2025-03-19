f21fb6a4c730d229b056df9517c23cde55fbf047
Tim Velasquez <Timmyv1031@gmail.com>
2025-03-18 21:27:15 -0500

    added models ts files

--- server/src/models/tasks.ts
+++ server/src/models/tasks.ts
@@ -1,1 +1,29 @@
-import { Schema, model, type Document} from 'mongoose';
+import { Schema, type Document } from 'mongoose';
+
+export interface TaskDocument extends Document {
+  taskId: string;
+  titles: string;
+  description: string;
+  stressLevel: string;
+}
+
+const taskSchema = new Schema<TaskDocument>({
+  description: {
+    type: String,
+    required: true,
+  },
+  taskId: {
+    type: String,
+    required: true,
+  },
+  stressLevel: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  }
+});
+
+export default taskSchema;
