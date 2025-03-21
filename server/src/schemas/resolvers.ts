d28f19690d1ec03fb023f74aae7f02563024eb52
Tim Velasquez <Timmyv1031@gmail.com>
2025-03-20 21:02:20 -0500

    committing updates to the resolver.

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -2,25 +2,8 @@ import User, { UserDocument } from '../models/User';
 import Tasks, {TaskDocument} from '../models/task'
 import { signToken } from '../services/auth';
 
-// interface Profile {
-//   _id: string;
-//   name: string;
-//   email: string;
-//   password: string;
-//   skills: string[];
-// }
-
-// interface AddSkillArgs {
-//   profileId: string;
-//   skill: string;
-// }
-
-// interface RemoveSkillArgs {
-//   profileId: string;
-//   skill: string;
-// }
-
 interface User{
+  _id: string;
   username: string;
   email: string;
   password: string;
@@ -54,6 +37,15 @@ interface Context {
   user?: User;
 }
 
+interface AddSkillArgs {
+  userId: string;
+  skill: string;
+}
+
+interface RemoveSkillArgs {
+  userId: string;
+  skill: string;
+}
 
 const resolvers = {
   Query: {
@@ -73,31 +65,29 @@ const resolvers = {
   Mutation: {
     addProfile: async (_parent: any, { input }: AddUserArgs): Promise<{ token: string; user: User }> => {
       const user = await User.create({ ...input });
-      const token = signToken(user.username, user.email, user._id, );
+      const token = signToken(user.username, user.email, user._id, user.wife);
       return { token, user };
     },
     login: async (_parent: any, { email, password }: { email: string; password: string }): Promise<{ token: string; user: User }> => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw AuthenticationError;
+        throw new AuthenticationError('User not found');
       }
 
-      const isPasswordHusband = await user.isPasswordHusband(password);
-      const isPasswordWife = await user.isPasswordWife(password);
-
-      if (!isPasswordHusband && !isPasswordWife) {
-        throw AuthenticationError;
+      const isPasswordValid = await user.Password(password);
+      if (!isPasswordValid) {
+        throw new AuthenticationError('Invalid password');
       }
 
-      const token = signToken(profile.name, profile.email, profile._id, isPasswordWife);
-      return { token, profile };
+      const token = signToken(user.name, user.email, user._id, user.password);
+      return { token, user };
     },
-    addTask: async (_parent: any, { profileId, skill }: AddSkillArgs, context: Context): Promise<Profile | null> => {
+    addTask: async (_parent: any, { userId, skill }: AddSkillArgs, context: Context): Promise<User | null> => {
       if (context.user) {
 
         if(context.user.wife) {
-          return await Profile.findOneAndUpdate(
-            { _id: profileId },
+          return await User.findOneAndUpdate(
+            { _id: userId },
             {
               $addToSet: { skills: skill },
             },
@@ -107,21 +97,23 @@ const resolvers = {
             }
           );
         } else {
-
+          throw new AuthenticationError('Only the wife can create, modify, or delete tasks');
         }
         
       }
       throw AuthenticationError;
     },
-    removeProfile: async (_parent: any, _args: any, context: Context): Promise<Profile | null> => {
+    // Maybe not needed - Keeping just in case.
+    // At this time, we do not have a remove profile functionality.
+    removeProfile: async (_parent: any, _args: any, context: Context): Promise<User | null> => {
       if (context.user) {
-        return await Profile.findOneAndDelete({ _id: context.user._id });
+        return await User.findOneAndDelete({ _id: context.user._id });
       }
       throw AuthenticationError;
     },
-    removeSkill: async (_parent: any, { skill }: RemoveSkillArgs, context: Context): Promise<Profile | null> => {
+    removeSkill: async (_parent: any, { skill }: RemoveSkillArgs, context: Context): Promise<User | null> => {
       if (context.user) {
-        return await Profile.findOneAndUpdate(
+        return await User.findOneAndUpdate(
           { _id: context.user._id },
           { $pull: { skills: skill } },
           { new: true }
